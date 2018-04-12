f2fcd0044ba4e7cedaa6b89b9b9c467eb16be61a
Jerson Moreno <jersonmr@gmail.com>
2018-04-12 09:52:52 -0400

    agregados algunos estilos base al perfil del cargo

--- resources/views/partials/landing/cargos/subcargos/tecnico-apoyo-en-asuntos-legales.blade.php
+++ resources/views/partials/landing/cargos/subcargos/tecnico-apoyo-en-asuntos-legales.blade.php
@@ -1,14 +1,19 @@
-<h1 class="font-weight-bold text-uppercase">TÉCNICO APOYO EN ASUNTOS LEGALES</h1>
-<h2 class="font-weight-bold text-uppercase">FUNCIÓN GENERAL DEL CARGO</h2>
+<h1 class="font-weight-bold text-uppercase" style="font-size:1.5rem;">TÉCNICO APOYO EN ASUNTOS LEGALES</h1>
+<hr>
+<h2 class="font-weight-bold text-uppercase" style="font-size:1.25rem;">FUNCIÓN GENERAL DEL CARGO</h2>
 <p>Desarrollar las actividades inherentes a la asesoría jurídica en la contratación del personal y respuesta de las solicitudes de carácter legal inherentes para su posterior revisión en caso de ser necesario.</p>
-<h2 class="font-weight-bold text-uppercase">EDUCACIÓN Y CONOCIMIENTOS</h2>
+<h2 class="font-weight-bold text-uppercase" style="font-size:1.25rem;">EDUCACIÓN Y CONOCIMIENTOS</h2>
 <p>Mínimo ocho (8) semestres de carrera profesional en Derecho.</p>
-<h2 class="font-weight-bold text-uppercase">EXPERIENCIA</h2>
+<h2 class="font-weight-bold text-uppercase" style="font-size:1.25rem;">EXPERIENCIA</h2>
 <p>Un (1) años en cargos relacionados</p>
-<h2 class="font-weight-bold text-uppercase">FUNCIONES ESPECIFICAS</h2>
+<h2 class="font-weight-bold text-uppercase" style="font-size:1.25rem;">FUNCIONES ESPECIFICAS</h2>
 <ul>
   <li>Tramitar Derechos de Petición y reclamaciones que sean elevadas al Grupo Gestión del Talento Humano.</li>
   <li>Dar respuesta a los Derechos de Petición y radicaciones en temas pensionales y de seguridad social inherentes al Grupo Gestión del Talento Humano.</li>
   <li>Proyectar Órdenes Presidenciales referentes a reconocimientos de pensiones e indemnizaciones sustitutivas.</li>
   <li>Recopilar y consolidar información para dar respuestas a solicitudes elevadas por la Oficina Asesora Jurídica.</li>
 </ul>
+
+<hr>
+
+<a href="#" class="btn btn-success">Postularme al cargo</a>
